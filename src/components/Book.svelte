a45c0ee9469e8eb20cddedd16a2cfc8581cc4ae9
Kris <kriskuiper74@gmail.com>
2020-03-03 09:27:05 +0100

    feat: implement cleaning books

--- src/components/Book.svelte
+++ src/components/Book.svelte
@@ -3,7 +3,13 @@
 </script>
 
 <article>
-    <h3>{book.titles[0]}</h3>
+    <h3>{book.title}</h3>
+    
+    {#each book.formats as format}
+        <p>{format}</p>
+    {/each}
+
+    <p>{book.isbn}</p>
 </article>
 
 <style>
