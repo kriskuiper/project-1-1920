4836e75c7e873d3ca8aedd2357c565e2c7bdd5bf
Kris <kriskuiper74@gmail.com>
2020-03-03 17:18:08 +0100

    refactor(app): clean up questions and placeholders

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
 	import api from '../lib/api-instance'
+	import { questions, placeholders } from './voice-config'
+
 	import Book from './components/Book.svelte'
 	import SpeechInput from './components/SpeechInput.svelte'
 
@@ -9,22 +11,6 @@
 	const handleResult = (value, transcript) => {
 		userAnswer = value
 		userTranscript = transcript.trim()
-	}
-
-	const questions = {
-		start: 'Heb je zin in je werkstuk?',
-		nee: 'Ugh, balen! Waar gaat je werkstuk over?',
-		ja: 'Awesome! Laten we snel beginnen. Waar gaat je werkstuk over?',
-		searchbooks: 'Cool! Ik ga meteen voor je op zoek',
-		unknown: 'Rustig aan, zo slim ben ik ook weer niet'
-	}
-
-	const placeholders = {
-		start: 'Ja of nee?',
-		nee: 'Zeg in één woord waar je werkstuk over gaat',
-		ja: 'Zeg in één woord waar je werkstuk over gaat',
-		searchbooks: 'Dit kan even duren...',
-		unknown: 'Ik vergeet ook nog eens alles, waar ging je werkstuk ook alweer over?'
 	}
 </script>
 
@@ -61,9 +47,5 @@
 
 	@media (min-width: 987px) {
 		h1 { font-size: 3rem }
-	}
-
-	p {
-		font-size: 1.225rem;
 	}
 </style>
