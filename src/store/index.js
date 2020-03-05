bf84ed96d0768fea7d90ca381c5c6ac70b6897e5
Kris <kriskuiper74@gmail.com>
2020-03-05 09:48:24 +0100

    feat(store): use description instead of intro

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store'
 
 export const assignmentTitle = writable('Henkie')
-export const assignmentIntro = writable('Mijn intro')
+export const assignmentDescription = writable('Mijn intro')
