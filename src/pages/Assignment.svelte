54a9f9b0865558d770bb60f4369d66b483a3a64f
Kris <kriskuiper74@gmail.com>
2020-03-05 12:37:06 +0100

    feat(assignment): render books in assignment

--- src/pages/Assignment.svelte
+++ src/pages/Assignment.svelte
@@ -1,8 +1,19 @@
 <script>
-    import { assignmentTitle, assignmentDescription } from '../store'
+    import {
+        assignmentTitle,
+        assignmentDescription,
+        assignmentBooks
+    } from '../store'
+
+    import Book from '../components/Book.svelte'
 </script>
 
 <main>
     <h1>{$assignmentTitle}</h1>
     <p>{$assignmentDescription}</p>
+    <ul>
+        {#each $assignmentBooks as book}
+            <Book book={book} />
+        {/each}
+    </ul>
 </main>
