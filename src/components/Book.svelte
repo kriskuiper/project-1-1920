2e419fd2e522abf39b659602fc552cefcd6d14e8
Kris <kriskuiper74@gmail.com>
2020-03-05 12:47:25 +0100

    feat(book): make it possible to remove on speak

--- src/components/Book.svelte
+++ src/components/Book.svelte
@@ -11,9 +11,21 @@
             const { transcript } = event.results[0][0]
             
             if (transcript === 'voeg toe') {
-                console.log('Toegevoegd')
-
                 addBookToAssignment(book)
+            }
+            
+            recognition.stop()
+        }
+    }
+
+    const removeFromAssignment = () => {
+        recognition.start()
+
+        recognition.onresult = (event) => {
+            const { transcript } = event.results[0][0]
+            
+            if (transcript.toLowerCase().includes('ver')) {
+                removeBookFromAssignment(book)
             }
             
             recognition.stop()
@@ -24,13 +36,21 @@
         assignmentBooks.set([...$assignmentBooks, bookToAdd])
     }
 
+    const removeBookFromAssignment = (bookToRemove) => {
+        const filtered = $assignmentBooks.filter(assignmentBook => {
+            return assignmentBook.isbn !== book.isbn
+        })
+        
+        assignmentBooks.set(filtered)
+    }
+
     $: isAdded = $assignmentBooks.some(assignmentBook => {
         return assignmentBook.isbn === book.isbn
     })
 </script>
 
 <article
-    on:click={addToAssignment}
+    on:click={isAdded ? removeFromAssignment : addToAssignment}
     class={isAdded ? 'is-added' : null}
 >
     {#if book.images}
