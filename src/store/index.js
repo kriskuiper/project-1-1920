7e564b3fca53fca6a8a3e07614b746847efda007
Kris <kriskuiper74@gmail.com>
2020-03-05 12:36:37 +0100

    feat(store): add assignmentBooks writable

--- src/store/index.js
+++ src/store/index.js
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store'
 
 export const assignmentTitle = writable('Henkie')
 export const assignmentDescription = writable('Mijn intro')
+export const assignmentBooks = writable([])
